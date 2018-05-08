8312994b61b4c19fbbc15e44492f3ef50ec44750
rigwild <contact@asauvage.fr>
2018-05-08 14:47:17 +0200

    promise skills

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,38 +1,77 @@
-//Get exercices and append them to table 
-function getExercices(dataTable) {
-	var request = $.ajax({
+let log = (el) => {
+	console.log(el)
+}
+
+const toObj = (array) => {
+	let obj = {}
+	array.forEach(x => obj[x.id] = x)
+	return obj
+};
+
+let skills
+
+//Fetch skills
+const getSkills = () => {
+	return new Promise((resolve, reject) => {
+		let request = $.ajax({
+			//url: "/skills",
+			url: "php_test_api/skills.php",
+			method: "GET",
+			dataType: "json"
+		})
+		request.done(response => {
+			log("Success fetching skills.")
+			resolve(toObj(response))
+		})
+		request.fail((jqXHR, textStatus) => {
+			//doSomething
+			reject(new Error("Fail fetching skills : " + jqXHR.status))
+		})
+	})
+}
+
+const skillsPromise = getSkills()
+	.then(resolve => {
+		skills = resolve
+	})
+	.catch(reject => {
+		log(reject.message)
+	})
+
+log(skills)
+
+//Fetch exercices and append them to table 
+const getExercices = dataTable => {
+	let request = $.ajax({
 		//url: "/exercices",
 		url: "php_test_api/exercices.php",
 		method: "GET",
-		dataType: "json",
-	});
-	request.done(function(response) {
-		console.log("Success fetching exercices.");
-		dataTable.clear().draw(); //Emptying the table (loading message)
-		setExercices(dataTable, response);
-	});
-	request.fail(function(jqXHR, textStatus) {
-		console.log("Fail fetching exercices : " + jqXHR.status);
-		dataTable.clear().draw();
-		alert("Impossible de charger les exercices.");
+		dataType: "json"
+	})
+	request.done(response => {
+		log("Success fetching exercices.")
+		dataTable.clear().draw() //Emptying the table (loading message)
+		setExercices(dataTable, response)
+	})
+	request.fail((jqXHR, textStatus) => {
+		log("Fail fetching exercices : " + jqXHR.status)
+		dataTable.clear().draw()
 		//doSomething
-	});
+	})
 }
 
 //Set exercices to the table
-function setExercices(dataTable, exercices) {
-	var tr, id, name, description, score, skills_unlocked, language;
-	var rowsToAdd = [];
-
-	for (var work in exercices) {
-		id = parseInt(work) + 1;
-		name = exercices[work].name;
-		description = exercices[work].description;
-		score = exercices[work].score;
-		skills_unlocked = exercices[work].skills_unlocked;
-		language = exercices[work].language;
-
-		rowsToAdd[work] = [id, name, description, score, "skills_unlocked", language];
-	}
-	dataTable.rows.add(rowsToAdd).draw(false);
+const setExercices = (dataTable, exercices) => {
+	let count = 0
+	dataTable.rows.add(
+		exercices.map(x => [
+			++count,
+			x.id,
+			x.name,
+			x.description,
+			x.score,
+			x.skills_unlocked,
+			x.language
+		])
+	).draw(false)
 }
