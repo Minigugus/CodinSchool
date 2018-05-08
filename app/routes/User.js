91ac9f049673460b661760c2dcf65684681675e2
rigwild <contact@asauvage.fr>
2018-05-09 00:16:30 +0200

    Ajout getExercices async, modification page login, logout, affichage nom en fonction du cookie(+ajout cookie dans user.js du serveur)

--- app/routes/User.js
+++ app/routes/User.js
@@ -4,12 +4,23 @@ const express = require('express');
 
 const router = express.Router();
 
+const testJson = {
+	id: 42,
+	name: "DUPONT Robert"
+}
+
 router.post('/login', (req, res) => {
-	res.status(200).cookie('codingschool_session', 'fake').send();
+	res.status(200)
+		.cookie('codingschool_session', 'fake')
+		.cookie('codingschool_user', JSON.stringify(testJson))
+		.send();
 	//res.status(403).send();
 });
 router.get('/logout', (req, res) => {
-	res.status(200).clearCookie('codingschool_session').send();
+	res.status(200)
+		.clearCookie('codingschool_session')
+		.clearCookie('codingschool_user')
+		.send();
 });
 
 module.exports = router;
