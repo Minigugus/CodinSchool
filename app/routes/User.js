f810176a3420e8082709e22f39ca8480e584a7f8
rigwild <contact@asauvage.fr>
2018-05-08 18:27:48 +0200

    Page de login + fix server User en POST

--- app/routes/User.js
+++ app/routes/User.js
@@ -4,8 +4,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
+router.post('/login', (req, res) => {
 	res.status(200).cookie('codingschool_session', 'fake').send();
+	//res.status(403).send();
 });
 router.get('/logout', (req, res) => {
 	res.status(200).clearCookie('codingschool_session').send();
