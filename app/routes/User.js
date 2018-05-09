54d650065ef2ce1cb9896e739fcae4780b83ac86
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-05-09 15:14:51 +0200

    Authentification fonctionnelle + ajout d'une route pour créer un compte.

--- app/routes/User.js
+++ app/routes/User.js
@@ -1,23 +1,31 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
 const db = require('../db');
 const session = require('../session.js');
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-	db.queryFirst('SELECT id, password_hash, name FROM account WHERE username = $1;', [ req.body.username ])
-		.then(user => ({ success: bcrypt.compare(req.body.password, user.password_hash), user: user }))
-		.then(result => {
-			result.success && (req.session.account_id = result.user.id);
-			res.status(result.success ? 200 : 403).json({ id: result.user.id, name: result.user.name });
-		})
-		.catch(err => {
-			console.error(`ERROR Login failed : ${err}`);
-			res.status(505).json({ message: 'Internal server error.' });
-		});
+const jsonParser = bodyParser.json();
+const urlParser = bodyParser.urlencoded({ extended: false });
+
+router.post('/login', urlParser, (req, res) => {
+	if (!req.body || !req.body.username || !req.body.password)
+		res.status(401).json({ message: 'Invalid request.' });
+	else
+		db.queryFirst('SELECT id, password_hash, name FROM account WHERE username = $1;', [ req.body.username ])
+			.then(user => ({ success: bcrypt.compare(req.body.password, user.password_hash), user: user }))
+			.then(result => {
+				result.success && (req.session.account_id = result.user.id);
+				res.status(result.success ? 200 : 403).json({ id: result.user.id, name: result.user.name });
+			})
+			.catch(err => {
+				console.error(`ERROR Login failed : ${err}`);
+				res.status(505).json({ message: 'Internal server error.' });
+			});
 });
 router.get('/logout', (req, res) => {
 	if (!session.isValid(req, res))
@@ -28,6 +36,20 @@ router.get('/logout', (req, res) => {
 				console.error(`ERROR Can't destroy session : ${err}`);
 			res.status(200).json({ });
 		});
+});
+
+// DEV
+router.post('/register', urlParser, (req, res) => {
+	if (!req.body || !req.body.username || !req.body.password || !req.body.name)
+		res.status(401).json({ message: 'Invalid request.' });
+	else
+		bcrypt.hash(req.body.password, 10)
+			.then(password_hash => db.queryFirst('INSERT INTO account (username, password_hash, name) VALUES ($1, $2, $3) RETURNING *;', [ req.body.username, password_hash, req.body.name ]))
+			.then(user => res.status(200).json({ id: user.id, name: user.name }))
+			.catch(err => {
+				console.error(`ERROR Register failed : ${err}`);
+				res.status(505).json({ message: 'Internal server error.' });
+			});
 });
 
 module.exports = router;
--- bin/codingschool.sh
+++ bin/codingschool.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd $(dirname $(dirname $(readlink -f $0)))
+
+DB_HOST=localhost \
+DB_USER=codinschool \
+DB_PASS=dev \
+DB_NAME=codinschool \
 node app/index.js
