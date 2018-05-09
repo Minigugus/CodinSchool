12b3c13d916c3f31986572c5fd6b6c327e336b48
rigwild <contact@asauvage.fr>
2018-05-09 20:17:20 +0200

    fonction fetch améliorée merge d'urgence.. florentin pas ouf...

--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -1,3 +1,8 @@
+/********** UTILITIES / CONFIG **********/
+
+//Api url
+let apiBaseUrl = "/api/"
+
 //DataTables french translation
 let dataTableSettings = {
     language: {
@@ -37,45 +42,80 @@ const toObj = (array) => {
 }
 
 
-//Fetch skills (Promise)
-const skillsPromise = () => {
+//Fetch anything (Promise)
+const getPromiseAPI = (apiUrl, httpMethod) => {
     return new Promise((resolve, reject) => {
         let request = $.ajax({
-            url: "/api/skills",
-            method: "GET",
+            url: apiBaseUrl + apiUrl,
+            method: httpMethod || "GET",
             dataType: "json"
         })
         request.done(response => {
-            log("Success fetching skills.")
+            log("Success fetching " + apiUrl + ".")
             resolve(toObj(response))
         })
         request.fail((jqXHR, textStatus) => {
-            //doSomething
-            reject(new Error("Fail fetching skills : " + jqXHR.status))
+            reject(new Error("Fail fetching " + apiUrl + " : " + jqXHR.status))
         })
     })
 }
 
-//Fetch languages (Promise)
-const languagesPromise = () => {
-    return new Promise((resolve, reject) => {
-        let request = $.ajax({
-            url: "/api/languages",
-            method: "GET",
-            dataType: "json"
-        })
-        request.done(response => {
-            //Fake timeout pour test !
-            setTimeout(() => {
-                log("Success fetching languages.")
-                resolve(toObj(response))
-            }, 3000)
-        })
-        request.fail((jqXHR, textStatus) => {
-            //doSomething
-            reject(new Error("Fail fetching languages : " + jqXHR.status))
-        })
-    })
+//Get cookie string by name
+const getCookie = sKey => {
+    if (!sKey) {
+        return null
+    }
+    let reg = new RegExp("(?:(?:^|.*;)\\s*" +
+        encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") +
+        "\\s*\\=\\s*([^;]*).*$)|^.*$")
+    return decodeURIComponent(document.cookie.replace(reg, "$1")) || null
+}
+
+//Get cookie object if cookie is json
+const getCookieObj = cname => {
+    let json
+    try {
+        json = JSON.parse(getCookie(cname))
+    } catch (e) {
+        return null
+    }
+    return json
+}
+
+const getSessionStorageObj = key => JSON.parse(sessionStorage.getItem(key))
+const setSessionStorageObj = (key, obj) => sessionStorage.setItem(key, JSON.stringify(obj))
+const clearSessionStorage = () => sessionStorage.clear()
+
+const getUrlParameters = () => {
+    let parts = window.location.search.substr(1).split("&"),
+        data = {}
+    for (let i = 0, c = parts.length; i < c; i++) {
+        let temp = parts[i].split("=")
+        data[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1])
+    }
+    return data
+}
+
+
+/********** Main functions **********/
+
+const fetchToSessionStorage = () => {
+    if (!getSessionStorageObj("user") || !getSessionStorageObj("skills") || !getSessionStorageObj("languages")) {
+        //Fetch asynchronously
+        (async () => {
+            try {
+                const res = await Promise.all([
+                    getPromiseAPI("user"),
+                    getPromiseAPI("skills"),
+                    getPromiseAPI("languages")
+                ])
+                //blabla
+            } catch (err) {
+                console.error(e)
+                return false
+            }
+        })()
+    }
 }
 
 //Fetch exercices 
@@ -98,6 +138,8 @@ const getExercices = dataTable => {
 
 //Set exercices to the table
 const setExercices = (dataTable, exercices) => {
+    dataTable.clear().draw()
+    setToTable(res[0], res[1])
     //Set everything to the table
     const setToTable = (fetchedSkills, fetchedLanguages) => {
         let skillsString = ""
@@ -122,22 +164,6 @@ const setExercices = (dataTable, exercices) => {
             ])
         ).draw(false)
     }
-
-    //Fetch asynchronously
-    (async () => {
-        Promise.all([
-                skillsPromise(),
-                languagesPromise()
-            ])
-            .catch(e => {
-                console.error(e)
-                return false
-            })
-            .then(res => {
-                dataTable.clear().draw()
-                setToTable(res[0], res[1])
-            })
-    })()
 }
 
 //Check login form
@@ -237,27 +263,6 @@ const logout = () => {
     })
 }
 
-//Get cookie string by name
-const getCookie = sKey => {
-    if (!sKey) {
-        return null
-    }
-    let reg = new RegExp("(?:(?:^|.*;)\\s*" +
-        encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") +
-        "\\s*\\=\\s*([^;]*).*$)|^.*$")
-    return decodeURIComponent(document.cookie.replace(reg, "$1")) || null
-}
-
-//Get cookie object if cookie is json
-const getCookieObj = cname => {
-    let json
-    try {
-        json = JSON.parse(getCookie(cname))
-    } catch (e) {
-        return null
-    }
-    return json
-}
 
 
 //how to check if session valid ?
@@ -274,14 +279,4 @@ const checkLoggedIn = () => {
         alert("Not logged in.\nCookies : " + document.cookie)
         throw new Error("User is not logged in !")
     }
-}
-
-const getUrlParameters = () => {
-    let parts = window.location.search.substr(1).split("&"),
-        data = {};
-    for (let i = 0, c = parts.length; i < c; i++) {
-        let temp = parts[i].split("=")
-        data[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1])
-    }
-    return data
 }
