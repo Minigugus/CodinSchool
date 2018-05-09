b169df4ab3c0c853b06b56f16dfe0f983a65c472
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-05-09 19:33:09 +0200

    Ajout de `POST /api/password` et `GET /api/login`.

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 -- from module `connect-pg-simple`.
-CREATE TABLE "session" (
+CREATE OR REPLACE TABLE "session" (
 	"sid" varchar NOT NULL COLLATE "default",
 	"sess" json NOT NULL,
 	"expire" timestamp(6) NOT NULL
@@ -7,9 +7,14 @@ CREATE TABLE "session" (
 WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
-CREATE TABLE account (
-	id serial PRIMARY KEY,
-	username varchar(24) NOT NULL UNIQUE,
-	password_hash char(60) NOT NULL,
-	name varchar(100) NOT NULL
+CREATE OR REPLACE TABLE account (
+	acc_id serial PRIMARY KEY,
+	acc_username varchar(24) NOT NULL UNIQUE,
+	acc_password_hash char(60) NOT NULL,
+	acc_name varchar(100) NOT NULL
+);
+
+CREATE OR REPLACE TABLE language (
+	lan_id varchar(10) PRIMARY KEY,
+	lan_name varchar(30)
 );
