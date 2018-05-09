6278a57e5c783090c4f342ff287e174e4dccaee3
rigwild <contact@asauvage.fr>
2018-05-10 01:27:44 +0200

    fix sql+User.js, ajout register et améliorations diverses

--- app/routes/User.js
+++ app/routes/User.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcrypt');
 
 const db = require('../db');
 const util = require('../util');
+const config = require('../config.js');
 
 const router = express.Router();
 
@@ -13,7 +14,7 @@ const jsonParser = bodyParser.json();
 const urlParser = bodyParser.urlencoded({ extended: false });
 
 router.get('/login', util.check, (req, res) => {
-	db.queryFirst('SELECT id, name FROM account WHERE id = $1;', [ req.session.account_id ])
+	db.queryFirst('SELECT acc_id, acc_name FROM account WHERE acc_id = $1;', [ req.session.account_id ])
 		.then(user => (user || Promise.reject(new Error(`Account ID ${req.session.account_id} not found !`))))
 		.then(user => res.status(200).json(user))
 		.catch(err => {
@@ -23,7 +24,7 @@ router.get('/login', util.check, (req, res) => {
 });
 
 router.post('/login', urlParser, util.validate('username', 'password'), (req, res) => {
-	db.queryFirst('SELECT id, password_hash, name FROM account WHERE username = $1;', [ req.body.username ])
+	db.queryFirst('SELECT acc_id, acc_password_hash, acc_name FROM account WHERE acc_username = $1;', [ req.body.username ])
 		.then(user => ({ success: user && bcrypt.compare(req.body.password, user.password_hash), user: user }))
 		.then(result => {
 			result.success && (req.session.account_id = result.user.id);
@@ -39,10 +40,13 @@ router.post('/login', urlParser, util.validate('username', 'password'), (req, re
 		});
 });
 router.post('/password', util.check, urlParser, util.validate('old_password', 'password'), (req, res) => {
-	db.queryFirst('SELECT password_hash FROM account WHERE id = $1;', [ req.session.account_id ])
+	db.queryFirst('SELECT acc_password_hash FROM account WHERE acc_id = $1;', [ req.session.account_id ])
 		.then(user => (user && bcrypt.compare(req.body.old_password, user.password_hash)))
 		.then(success => (success ? res.status(403).json({ message: 'Wrong old password.' }) : bcrypt.hash(req.body.password, config.bcrypt_rounds)
-			.then(password_hash => db.execute('UPDATE account SET password_hash = $1;', [ req.body.password ]))
+
+			//Whaaaat ? attention !!! tu écrases les hash de tout le monde !! xD oublie pas le where.
+			.then(password_hash => db.execute('UPDATE account SET acc_password_hash = $1;', [ req.body.password ]))
+			
 			.then(rowCount => {
 				res.status(204).end();
 			})))
@@ -62,7 +66,7 @@ router.get('/logout', util.check, (req, res) => {
 // DEV
 router.post('/register', urlParser, util.validate('username', 'password', 'name'), (req, res) => {
 	bcrypt.hash(req.body.password, config.bcrypt_rounds)
-		.then(password_hash => db.queryFirst('INSERT INTO account (username, password_hash, name) VALUES ($1, $2, $3) RETURNING *;', [ req.body.username, password_hash, req.body.name ]))
+		.then(password_hash => db.queryFirst('INSERT INTO account (acc_username, acc_password_hash, acc_name) VALUES ($1, $2, $3) RETURNING *;', [ req.body.username, password_hash, req.body.name ]))
 		.then(user => res.status(200).json({ id: user.id, name: user.name }))
 		.catch(err => {
 			console.error(`ERROR Register failed : ${err}`);
--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 -- from module `connect-pg-simple`.
-CREATE OR REPLACE TABLE "session" (
+CREATE TABLE "session" (
 	"sid" varchar NOT NULL COLLATE "default",
 	"sess" json NOT NULL,
 	"expire" timestamp(6) NOT NULL
@@ -7,14 +7,14 @@ CREATE OR REPLACE TABLE "session" (
 WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
-CREATE OR REPLACE TABLE account (
+CREATE TABLE account (
 	acc_id serial PRIMARY KEY,
 	acc_username varchar(24) NOT NULL UNIQUE,
 	acc_password_hash char(60) NOT NULL,
 	acc_name varchar(100) NOT NULL
 );
 
-CREATE OR REPLACE TABLE language (
+CREATE TABLE language (
 	lan_id varchar(10) PRIMARY KEY,
 	lan_name varchar(30)
 );
