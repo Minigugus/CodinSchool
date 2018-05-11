f38d5b50d01d789248e9ec317b7e59826de85d11
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-05-11 12:45:41 +0200

    Finalisation de la Base de Données

--- database.sql
+++ database.sql
@@ -1,5 +1,16 @@
--- from module `connect-pg-simple`.
+BEGIN;
+
 DROP TABLE IF EXISTS "session";
+DROP TABLE IF EXISTS account CASCADE;
+DROP TABLE IF EXISTS language CASCADE;
+DROP TABLE IF EXISTS skill CASCADE;
+DROP TABLE IF EXISTS exercice CASCADE;
+DROP TABLE IF EXISTS skill_unlockable CASCADE;
+DROP TABLE IF EXISTS exercice_test CASCADE;
+DROP TABLE IF EXISTS score_test CASCADE;
+DROP TABLE IF EXISTS score_exercice CASCADE;
+
+-- from module `connect-pg-simple`.
 CREATE TABLE "session" (
 	"sid" varchar NOT NULL COLLATE "default",
 	"sess" json NOT NULL,
@@ -8,7 +19,13 @@ CREATE TABLE "session" (
 WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
-DROP TABLE IF EXISTS account CASCADE;
+CREATE TABLE admin (
+	adm_id serial PRIMARY KEY NOT NULL,
+	adm_username varchar(24) NOT NULL UNIQUE,
+	adm_password_hash char(60) NOT NULL,
+	adm_name varchar(50) NOT NULL
+);
+
 CREATE TABLE account (
 	acc_id serial PRIMARY KEY NOT NULL,
 	acc_username varchar(24) NOT NULL UNIQUE,
@@ -16,7 +33,6 @@ CREATE TABLE account (
 	acc_name varchar(50) NOT NULL
 );
 
-DROP TABLE IF EXISTS language CASCADE;
 CREATE TABLE language (
 	lan_id varchar(10) PRIMARY KEY NOT NULL,
 	lan_name varchar(30) NOT NULL,
@@ -25,17 +41,49 @@ CREATE TABLE language (
 	lan_cmd_exec varchar(200) NOT NULL
 );
 
-DROP TABLE IF EXISTS skill CASCADE;
 CREATE TABLE skill (
 	ski_id integer PRIMARY KEY NOT NULL,
 	ski_name varchar(100) NOT NULL
 );
 
--- DROP TABLE IF EXISTS exercice CASCADE;
--- CREATE TABLE exercice (
--- 	exe_id varchar(20) PRIMARY KEY NOT NULL,
--- 	exe_name varchar(100) NOT NULL,
--- 	exe_description varchar(2000),
--- 	exe_language varchar(10) NOT NULL REFERENCES language (lan_id),
--- 	exe_skills integer[] NOT NULL DEFAULT '{}',
--- );
+CREATE TABLE exercice (
+	exe_id varchar(20) PRIMARY KEY NOT NULL,
+	exe_name varchar(100) NOT NULL,
+	exe_description varchar(2000),
+	exe_language varchar(10) NOT NULL REFERENCES language (lan_id),
+	exe_default_code varchar(5000) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE skill_unlockable (
+	exe_id varchar(20) NOT NULL REFERENCES exercice (exe_id),
+	ski_id integer NOT NULL REFERENCES skill (ski_id),
+	PRIMARY KEY(exe_id, ski_id)
+);
+
+CREATE TABLE exercice_test (
+	exe_id varchar(20) NOT NULL REFERENCES exercice (exe_id),
+	tes_id integer NOT NULL,
+	tes_exemple boolean NOT NULL DEFAULT false,
+	tes_input varchar(10000) NOT NULL DEFAULT '',
+	tes_output varchar(1000) NOT NULL DEFAULT '',
+	tes_description varchar(200),
+	PRIMARY KEY(exe_id, tes_id)
+);
+
+CREATE TABLE score_test (
+	acc_id integer NOT NULL REFERENCES account (acc_id),
+	exe_id varchar(20) NOT NULL,
+	tes_id integer NOT NULL,
+	sco_passed boolean NOT NULL,
+	PRIMARY KEY(acc_id, exe_id, tes_id),
+	FOREIGN KEY(exe_id, tes_id) REFERENCES exercice_test (exe_id, tes_id)
+);
+
+CREATE TABLE score_exercice (
+	acc_id integer NOT NULL REFERENCES account (acc_id),
+	exe_id varchar(20) NOT NULL REFERENCES exercice (exe_id),
+	sco_code varchar(5000) NOT NULL,
+	PRIMARY KEY(acc_id, exe_id)
+);
+
+COMMIT;
---
+++ dev.sql
@@ -0,0 +1,48 @@
+BEGIN;
+
+-- Mot de passe : dev
+INSERT INTO admin VALUES
+(42, 'test@gmail.com', '$2b$10$1.U0zUsJx9JitM7AHSJTUuM7KhtxkKDA.IqbX5/bf0cD5vnIPAqPi', 'DUPONT Robert');
+
+-- Mot de passe : dev
+INSERT INTO account VALUES
+(42, 'test@gmail.com', '$2b$10$1.U0zUsJx9JitM7AHSJTUuM7KhtxkKDA.IqbX5/bf0cD5vnIPAqPi', 'DUPONT Robert');
+
+INSERT INTO language VALUES
+('c', 'C', 'SRC.c', 'gcc -o DST SRC.c', './DST'),
+('java', 'Java', 'Main.java', 'javac Main.java', 'java Main');
+
+INSERT INTO skill VALUES
+(75, 'Utilisation de la classe Scanner'),
+(153, 'Manipulation des chaines de caractères'),
+(241, 'Utilisation de l''entrée standard'),
+(406, 'Utilisation de CodinSchool');
+
+INSERT INTO exercice VALUES
+('hello-world', 'Hello World !', 'Le premier programme que tout developpeur doit connaître !', 'c', '#include <stdio.h>\n\nint main(int argc, char const *argv[])\n{\n\tprintf("Votre message\\n");\n\treturn 0;\n}'),
+('somme-nombres', 'Somme de Nombres', 'Additionner 2 nombres choisis par l''utilisateur. C''est aussi simple que ça !', 'java', 'import java.io.Scanner;\n\nclass Main {\n\tpublic static void main(String[] args) {\n\t\tScanner sc = new Scanner(System.in);\n\t\tint nb = sc.nextInt();\n\t\tSystem.out.println("Nombre : " + nb);\n\t}\n}');
+
+INSERT INTO skill_unlockable VALUES
+('hello-world', 153),
+('hello-world', 406),
+('somme-nombres', 75),
+('somme-nombres', 153),
+('somme-nombres', 241);
+
+INSERT INTO exercice_test VALUES
+('hello-world', 0, true, '', 'Hello World!', 'Très compliqué...'),
+('somme-nombres', 0, true, '3\n1\n2\n3', '6', NULL),
+('somme-nombres', 1, false, '1\n1', '1', NULL),
+('somme-nombres', 2, false, '0', '0', NULL);
+
+INSERT INTO score_test VALUES
+(42, 'hello-world', 0, true),
+(42, 'somme-nombres', 0, true),
+(42, 'somme-nombres', 1, false),
+(42, 'somme-nombres', 2, false);
+
+INSERT INTO score_exercice VALUES
+(42, 'hello-world', '#include <stdio.h>\n\nint main(int argc, char const *argv[])\n{\n\tprintf("Hello World!\\n");\n\treturn 0;\n}'),
+(42, 'somme-nombres', 'import java.io.Scanner;\n\nclass Main {\n\tpublic static void main(String[] args) {\n\t\tScanner sc = new Scanner(System.in);\n\t\tint nb = sc.nextInt();\n\t\tint somme = sc.nextInt();\n\t\tfor (int i = 0; i <= nb; i++)\n\t\t{\n\t\t\tint n = sc.nextInt();\n\t\t\tsomme = somme + n;\n\t\t}\n\t\tSystem.out.println(somme);\n\t}\n}');
+
+COMMIT;
