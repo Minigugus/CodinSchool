51729ce473e1223d88329008483afa3e6eb57a0d
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-05-11 18:19:57 +0200

    Correction Skills

--- app/routes/Skill.js
+++ app/routes/Skill.js
@@ -8,8 +8,8 @@ const api = require('../api.js');
 const USER = express.Router(), ADMIN = express.Router();
 
 USER.get('/', api.check, (req, res) => 
-	db.queryAll('SELECT S.ski_id, S.ski_name, (SA.ski_level / SA.ski_level_max::float) AS ski_ratio FROM skill S INNER JOIN skill_account SA ON SA.ski_id=S.ski_id WHERE SA.acc_id = $1;', [ req.session.account_id ])
-		.then(rows => rows.map(x => ({ id: x.ski_id, name: x.ski_name, level: Number(x.ski_ratio.toFixed(2)) }))) // https://stackoverflow.com/questions/9453421
+	db.queryAll('SELECT S.ski_id, S.ski_name, CASE WHEN SA.ski_level IS NULL THEN 0 ELSE SA.ski_level::integer END, CASE WHEN SA.ski_level IS NULL THEN 0 ELSE SA.ski_level_max::integer END FROM skill S LEFT JOIN (SELECT * FROM skill_account WHERE acc_id = $1) SA ON S.ski_id=SA.ski_id;', [ req.session.account_id ])
+		.then(rows => rows.filter(x => x.ski_level_max).map(x => ({ id: x.ski_id, name: x.ski_name, level: Number((x.ski_level / x.ski_level_max).toFixed(2)) }))) // https://stackoverflow.com/questions/9453421
 		.then(skills => api.reply(res, 0, skills))
 );
 
