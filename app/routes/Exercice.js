0b74f70218fbd3e1e1e9cc765c923c804cc937f8
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-05-12 12:20:35 +0200

    Partie GET de l'API fonctionnelle

--- app/routes/Exercice.js
+++ app/routes/Exercice.js
@@ -23,25 +23,26 @@ USER.get('/', api.check, (req, res) =>
 		.then(exercices => api.reply(res, 0, exercices))
 		.catch(err => api.error(res, 'Get Exercices failed', err))
 );
-USER.get('/:id', api.check, (req, res) => {
-	api.reply(res, 0, {
-		id: "hello-world",
-		name: "Hello World !",
-		description: "Le premier programme que tout developpeur doit connaître !",
-		score: 1,
-		skills_unlocked: [ 153, 406 ],
-		language: "java",
-		nb_tests: 2,
-		best_code: "class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Hello World!\"); }\n\t}\n}",
-		exemples: [
-			{
-				input: "",
-				output: "Hello World!\n",
-				description: "Très compliqué..."
-			}
-		]
-	});
-});
+USER.get('/:id', api.check, api.validate('params', 'id'), (req, res) =>
+	db.queryFirst('SELECT E.exe_id, E.exe_name, E.exe_description, ES.exe_test_passed, E.exe_nb_tests, SU.exe_skills, E.exe_language, E.exe_default_code, (SELECT sco_code FROM exercice_best_eval WHERE exe_id=E.exe_id AND acc_id=ES.acc_id) AS sco_code FROM exercice_description E, exercice_score ES, (SELECT exe_id, ARRAY_AGG(ski_id) AS exe_skills FROM exercice_skill_unlockable GROUP BY exe_id) SU WHERE E.exe_id=SU.exe_id AND E.exe_id=ES.exe_id AND E.exe_id = $2 AND ES.acc_id = $1;', [ req.session.account_id, req.params.id ])
+		.then(e => (!e ? e : db.queryAll('SELECT * FROM exercice_test WHERE tes_exemple AND exe_id = $1;', [ e.exe_id ])
+			.then(rows => rows.map(x => ({ input: x.tes_input, output: x.tes_output, description: x.tes_description })))
+			.then(exemples => (
+				{
+					id: e.exe_id,
+					name: e.exe_name,
+					description: e.exe_description,
+					score: Number((e.exe_test_passed / e.exe_nb_tests).toFixed(2)),
+					nb_tests: e.exe_test_passed,
+					nb_tests_passed: e.exe_test_passed,
+					skills_unlocked: e.exe_skills,
+					language: e.exe_language,
+					best_code: e.sco_code,
+					exemples: exemples
+				}))))
+		.then(exercice => exercice ? api.reply(res, 0, exercice) : api.reply(res, 20, { id: req.params.id }))
+		.catch(err => api.error(res, `Get Exercice '${req.params.id}' failed`, err))
+);
 // router.post('/:id', (req, res) => { });
 
 module.exports = {
