c77bb341cbd895b789a5608a93af3daa020db29c
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-05-12 11:40:56 +0200

    Liste des exercices fonctionnelle

--- app/routes/Exercice.js
+++ app/routes/Exercice.js
@@ -8,13 +8,16 @@ const api = require('../api.js');
 const USER = express.Router(), ADMIN = express.Router();
 
 USER.get('/', api.check, (req, res) =>
-	db.queryAll('SELECT E.exe_id, E.exe_name, E.exe_description, ES.exe_test_passed, E.exe_nb_tests, E.exe_language AS exe_score FROM exercice_description E, exercice_score ES WHERE E.exe_id=ES.exe_id AND ES.acc_id=$1;', [ req.session.account_id ])
+	db.queryAll('SELECT E.exe_id, E.exe_name, E.exe_description, ES.exe_test_passed, E.exe_nb_tests, SU.exe_skills, E.exe_language FROM exercice_description E, exercice_score ES, (SELECT exe_id, ARRAY_AGG(ski_id) AS exe_skills FROM exercice_skill_unlockable GROUP BY exe_id) SU WHERE E.exe_id=SU.exe_id AND E.exe_id=ES.exe_id AND ES.acc_id=$1;', [ req.session.account_id ])
 		.then(rows => rows.map(x => (
 			{
 				id: x.exe_id,
 				name: x.exe_name,
 				description: x.exe_description,
 				score: Number((x.exe_test_passed / x.exe_nb_tests).toFixed(2)),
+				nb_tests: x.exe_test_passed,
+				nb_tests_passed: x.exe_test_passed,
+				skills_unlocked: x.exe_skills,
 				language: x.exe_language
 			})))
 		.then(exercices => api.reply(res, 0, exercices))
