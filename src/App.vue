b93fdea9a7eac28173d2fd74462aebf049ee98cb
rigwild <contact@asauvage.fr>
2018-07-11 23:15:42 +0200

    deleted all. installed vuejs and new project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/login">About</router-link> |
+        <router-link to="/profile/247">Profile</router-link>
+      </div>
+    <router-view/>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+import Vuex from 'vuex'
+
+export default {
+  computed: {
+    ...Vuex.mapGetters([
+    ])
+  },
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
