331ce51640405759391a882430229b885932a87c
rigwild <contact@asauvage.fr>
2018-07-13 19:49:01 +0200

    Front-end base. routes, store, menu, login page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,57 @@
 <template>
   <div id="app">
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/login">About</router-link> |
-        <router-link to="/profile/247">Profile</router-link>
+    <div class="ui inverted menu">
+      <div class="ui container">
+        <router-link to="/" class="header item prevent-active">
+          <img class="logo" src="/static/img/favicon.png"> Codinschool
+        </router-link>
+        <router-link to="/" class="item">Accueil</router-link>
+        <router-link to="/login" class="item">Login</router-link>
+        <router-link to="/profile" class="item">Prenom S.</router-link>
       </div>
+    </div>
+
+    <div class="ui container">
+
+      <div v-if="notification.visible" v-bind:class="notification.type" class="ui message">
+        <i @click="closeNotification()" class="close icon"></i>
+        <div class="header">
+          {{ notification.header }}
+        </div>
+        <p>{{ notification.message }}</p>
+      </div>
+
     <router-view/>
+    </div>
   </div>
 </template>
+
+<script>
+import Vuex from 'vuex'
+
+export default {
+  computed: {
+    ...Vuex.mapGetters([
+      'notification'
+    ])
+  },
+  methods: {
+    ...Vuex.mapActions([
+      'closeNotification'
+    ])
+  }
+}
+</script>
+
+<style>
+  .prevent-active {
+    background: 0 0 !important;
+    color: rgba(255,255,255,.9) !important;
+  }
+  #app {
+    text-align: left;
+    font-family: Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>This is the profile page</h1>
+  </div>
+</template>
+
+<script>
+import Vuex from 'vuex'
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      },
+      notification: {
+        visible: false,
+        message: ''
+      }
+    }
+  },
+  computed: {
+    ...Vuex.mapGetters([
+      'API_ROUTES'
+    ])
+  },
+  methods: {
+    ...Vuex.mapActions([
+      'setUserData'
+    ]),
+    setErrorVisible (b) { // Show/hide error message
+      this.error.visible = b
+    }
+  }
+}
+</script>
