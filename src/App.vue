b43f998397f6fa13a9c073a21fc8fb3a08b6ce4b
rigwild <contact@asauvage.fr>
2018-07-15 00:34:29 +0200

    Multiple notifications + transitions (need visual fix)

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,21 @@
 
     <div class="ui container">
 
-      <div v-if="notification.visible" v-bind:class="notification.type" class="ui message">
-        <i @click="closeNotification()" class="close icon"></i>
-        <div class="header">
-          {{ notification.header }}
-        </div>
-        <p>{{ notification.message }}</p>
-      </div>
+      <div class="ui button" @click="addNotification({type: 'info', message: Math.random().toString(36).substr(2, 5)})">Ajouter une notification</div>
 
-    <router-view/>
+      <transition name="fade">
+        <div v-if="notificationCount > 0" id="notification">
+          <transition-group name="list" mode="out-in">
+            <div v-for="(notif, index) in notification" :key="index" v-bind:class="notif.type" class="ui message list-item">
+              <i @click="closeNotification(index)" class="close icon"></i>
+              <div class="header">{{ notif.header }}</div>
+              <p>{{ notif.message }}</p>
+            </div>
+          </transition-group>
+        </div>
+      </transition>
+
+      <router-view/>
     </div>
   </div>
 </template>
@@ -32,11 +38,13 @@ import Vuex from 'vuex'
 export default {
   computed: {
     ...Vuex.mapGetters([
-      'notification'
+      'notification',
+      'notificationCount'
     ])
   },
   methods: {
     ...Vuex.mapActions([
+      'addNotification',
       'closeNotification'
     ])
   }
@@ -44,10 +52,52 @@ export default {
 </script>
 
 <style>
+  #notification {
+    margin-bottom: 20px !important;
+  }
+
+  /* START Notification container animation*/
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity .3s ease;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  /* END Notification container animation*/
+
+  /* START Notification list animation*/
+
+  .list-item {
+    transition: all 1s !important;
+    display: block;
+    width: 100%
+  }
+
+  .list-enter,
+  .list-leave-to {
+    opacity: 0 !important;
+  }
+
+  .list-leave-active {
+    position: absolute !important;
+  }
+
+  .list-move {
+    transition: transform 1s;
+  }
+
+  /* END Notification list animation*/
+
   .prevent-active {
     background: 0 0 !important;
-    color: rgba(255,255,255,.9) !important;
+    color: rgba(255, 255, 255, .9) !important;
   }
+
   #app {
     text-align: left;
     font-family: Arial, sans-serif;
--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+const notificationTypes = [
+  {type: 'info', header: 'Information'},
+  {type: 'warning', header: 'Attention'},
+  {type: 'success', header: 'Succès'},
+  {type: 'error', header: 'Erreur'}
+]
 const httpCodesList = {
   200: true,
   404: false
@@ -31,12 +37,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    notification: {
-      visible: true,
-      type: 'info',
-      header: 'Irure officia ex officia cillum sit duis voluptate sint est deserunt.',
-      message: 'Velit id nulla aliquip non. Lorem fugiat eiusmod commodo sunt deserunt ad ea velit reprehenderit Lorem ipsum pariatur enim magna. Ut nulla magna est do adipisicing cillum dolor nostrud sunt adipisici'
-    },
+    notification: [],
     apiCall,
     userData: {
       username: ''
@@ -44,32 +45,36 @@ export default new Vuex.Store({
   },
   getters: {
     API_ROUTES: () => API_ROUTES,
-    notification: state => { return state.notification }
+    notification: state => state.notification,
+    notificationCount: state => state.notification.length
   },
   mutations: {
-    SET_NOTIFICATION (state, type, content) {
-      console.log('Mutation : SET_NOTIFICATION=' + content)
-      state.notification.visible = true
-      state.notification.type = type || 'info'
-      state.notification.header = content || ''
-      state.notification.message = content || ''
+    ADD_NOTIFICATION (state, notif) {
+      let header = ''
+      // Check if type exists and gfet its corresponding header text
+      if (notif.type && notif.message && ({header} = notificationTypes.find(x => x.type === notif.type))) {
+        console.log(`Mutation : ADD_NOTIFICATION= ${notif.type} : ${notif.message}`)
+        state.notification.push({
+          type: notif.type,
+          header,
+          message: notif.message
+        })
+      }
     },
-    CLOSE_NOTIFICATION (state) {
-      console.log('Mutation : CLOSE_NOTIFICATION')
-      state.notification.visible = false
-      state.notification.type = 'info'
-      state.notification.header = ''
-      state.notification.message = ''
+    CLOSE_NOTIFICATION (state, index) {
+      if (state.notification.hasOwnProperty(index)) {
+        console.log('Mutation : CLOSE_NOTIFICATION=' + index)
+        state.notification.splice(index, 1)
+      }
     },
     SET_USER_DATA (state, data) {
       console.log('Mutation : SET_USER_DATA=' + data)
-      console.log(data)
     }
   },
   actions: {
-    setNotification: (store, type, header, content) => store.commit('SET_NOTIFICATION', type, header, content),
-    closeNotification: (store) => store.commit('CLOSE_NOTIFICATION'),
-    setUserData: (store, data) => store.commit('SET_USER_DATA', data)
+    addNotification: ({commit}, notif) => commit('ADD_NOTIFICATION', notif),
+    closeNotification: ({commit}, index) => commit('CLOSE_NOTIFICATION', index),
+    setUserData: ({commit}, data) => commit('SET_USER_DATA', data)
   },
   strict: true
 })
