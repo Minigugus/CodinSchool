5d78aff8927ffa2eb06c2ad2c9c25ee69757716a
rigwild <contact@asauvage.fr>
2018-07-17 15:28:41 +0200

    Profile page, need edit buttons

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="ui raised very padded text container segment">
     <h2 class="ui center aligned header">
-      <i class="settings icon" style="display: inline;position: absolute;margin-left: -44px;margin-top: 7px;"></i>
+      <i class="user icon" style="display: inline;position: absolute;margin-left: -44px;margin-top: 7px;"></i>
       <div class="content" style="display: inline-block;">
         Mon profil
-        <div class="sub header">{{ getUserData.username }}</div>
       </div>
     </h2>
+
+    <div class="ui items text-center">
+      <img class="ui circular image" :src="getUserData.avatar" style="display: inline;">
+      <div class="item">
+        <div class="content">
+          <a class="header">{{ `${getUserData.firstname} ${getUserData.lastname}` }}</a>
+          <div class="meta">
+            <span>Adresse email : {{ getUserData.email }}</span>
+          </div>
+          <div class="extra">Roles :
+            <span v-for="(role, index) in getUserData.roles" :key="index">{{ role }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,8 +35,7 @@ export default {
     ])
   },
   methods: {
-    ...Vuex.mapActions([
-    ])
+    ...Vuex.mapActions([])
   }
 }
 </script>
