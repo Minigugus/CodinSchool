2dbf61659d6b75da3e17d3eaa1c9b425d8b6e340
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-07-19 11:42:43 +0200

    Modification structure serveur

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 
+const db = require('./db');
 const config = require('./config');
 
 const app = express();
@@ -9,5 +10,16 @@ const app = express();
 app.set('trust proxy', 1);
 
 app.use(config.root, require('./routes'));
+
+db
+.sync()
+.then(() => {
+	app.listen(config.port, (err) => {
+		if (err)
+			console.error(`Server listen failed : ${err}`);
+		else
+			console.info(`Server listening port ${config.port}. http://localhost:${config.port}${config.root}`);
+	});
+});
 
 module.exports = app;
--- index.js
+++ index.js
@@ -2,17 +2,4 @@
 
 'use strict';
 
-const config = require('./app/config');
-const db = require('./app/db');
-const app = require('./app');
-
-db
-.sync()
-.then(() => {
-	app.listen(config.port, (err) => {
-		if (err)
-			console.error(`Server listen failed : ${err}`);
-		else
-			console.info(`Server listening port ${config.port}. http://localhost:${config.port}${config.root}`);
-	});
-});
+require('./app');
