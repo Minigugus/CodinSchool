8ea03245c3f2a4ca833edab1deee4f001980d004
rigwild <contact@asauvage.fr>
2018-07-19 18:55:45 +0200

    vue config + gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  lintOnSave: true,
+  outputDir: path.resolve(__dirname, "./app/public")
 }
