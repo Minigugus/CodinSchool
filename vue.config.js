3f601a7cef6b1d2089957a18a4fa288eb3201fe4
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-07-20 12:08:50 +0200

    Petites corrections

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const path = require('path')
 module.exports = {
   productionSourceMap: false,
   lintOnSave: true,
-  outputDir: path.resolve(__dirname, "./app/public")
+  outputDir: 'app/public'
 }
