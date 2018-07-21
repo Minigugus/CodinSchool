ce4a417e264544f66b3726e7bd503256dc8611ad
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-07-21 19:52:50 +0200

    Petites amélioration

--- app/api.js
+++ app/api.js
@@ -4,7 +4,7 @@ const { checkSchema, validationResult } = require('express-validator/check');
 
 const db = require('./db');
 
-const send = (res, code, body) => body ? res.status(code).json(body) : res.status(code).end();
+const send = (res, code, { message, ...body }) => body ? res.status(code, message).json(body) : res.status(code, message).end();
 
 const ok = (res, body) => send(res, body ? 200 : 204, body);
 
