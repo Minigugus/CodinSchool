d0ce55bdddf765fa251d8ef2a7d5ee0411081e69
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-07-21 15:39:03 +0200

    Désactivation du cache pour l'API + Content Securiy Policy

--- app/routes/api/login.js
+++ app/routes/api/login.js
@@ -38,38 +38,43 @@ module.exports = express.Router()
 		in: [ 'body' ],
 		isLength: {
 			errorMessage: 'Invalid password',
-			options: { min: 3, max: 30 }
+			options: { min: 3, max: 80 }
 		}
 	}
 }), (req, res) => {
-	const handle = () => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty())
-			send(res, 400, { errors: errors.array() });
-		else
-			User.findOne({ where: { email: req.body.email } })
-				.then(user => (user && bcrypt.compare(req.body.password, user.password_hash)
-					.then(success => (success ? user : null))))
-				.then(user => {
-					if (!user)
-						send(res, 403, { message: 'Invalid email or password' });
-					else
-					{
-						req.session.user_id = user.id;
-						ok(res, {
-							lastname: user.lastname,
-							firstname: user.firstname,
-							email: user.email,
-							enabled: user.enabled
-						});
-					}
-				})
-				.catch(err => send(res, 500, err));
-	};
-	res.format({
-		'application/json': handle,
-		'application/x-www-form-urlencoded': handle,
+	if (res.status !== 200)
+		res.end({ message: res.type });
+	else
+	{
+		const handle = () => {
+			const errors = validationResult(req);
+			if (!errors.isEmpty())
+				send(res, 400, { errors: errors.array() });
+			else
+				User.findOne({ where: { email: req.body.email } })
+					.then(user => (user && bcrypt.compare(req.body.password, user.password_hash)
+						.then(success => (success ? user : null))))
+					.then(user => {
+						if (!user)
+							send(res, 403, { message: 'Invalid email or password' });
+						else
+						{
+							req.session.user_id = user.id;
+							ok(res, {
+								lastname: user.lastname,
+								firstname: user.firstname,
+								email: user.email,
+								enabled: user.enabled
+							});
+						}
+					})
+					.catch(err => send(res, 500, err));
+		};
+		res.format({
+			'application/json': handle,
+			'application/x-www-form-urlencoded': handle,
 
-		'default': () => send(res, 406, { message: 'Format not supported' })
-	});
+			'default': () => send(res, 406, { message: 'Format not supported' })
+		});
+	}
 });
