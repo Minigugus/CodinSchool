8307a4f2196133e2764ca81ff36c5e2b8b348854
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2018-07-23 17:40:19 +0200

    Révision du fonctionnement de l'API côté Serveur.

--- app/routes/api/register.js
+++ app/routes/api/register.js
@@ -5,56 +5,67 @@ const bcrypt = require('bcrypt');
 const { checkSchema, validationResult } = require('express-validator/check');
 
 const config = require('../../config');
-const { send, ok } = require('../../api');
+const { send, ok, api, APIError } = require('../../api');
 const { User } = require('../../db');
 
 module.exports = express.Router()
-.use(express.json({ limit: '2kb' }))
-.use(express.urlencoded({ limit: '2kb', parameterLimit: 2 }))
-.post('/', checkSchema({
-	email: {
-		in: [ 'body' ],
-		errorMessage: 'Invalid email',
-		isEmail: true
-	},
-	password: {
-		in: [ 'body' ],
-		isLength: {
-			errorMessage: 'Invalid password',
-			options: { min: 3, max: 80 }
+.post('/',
+	express.json({ limit: '2kb' }), 
+	express.urlencoded({ limit: '2kb', parameterLimit: 2 }),
+	api({
+		guest: true,
+		validation: {
+			email: {
+				in: [ 'body' ],
+				errorMessage: 'Invalid email',
+				isEmail: true
+			},
+			password: {
+				in: [ 'body' ],
+				isLength: {
+					errorMessage: 'Invalid password',
+					options: { min: 3, max: 80 }
+				}
+			},
+			lastname: {
+				in: [ 'body' ],
+				isLength: {
+					errorMessage: 'Invalid lastname',
+					options: { min: 3, max: 30 }
+				}
+			},
+			firstname: {
+				in: [ 'body' ],
+				isLength: {
+					errorMessage: 'Invalid firstname',
+					options: { min: 3, max: 30 }
+				}
+			}
+		},
+		callback: async (req, res) => {
+			const password_hash = await bcrypt.hash(req.body.password, config.bcrypt_rounds);
+			try
+			{
+				const user = await User.create({
+					lastname: req.body.lastname,
+					firstname: req.body.firstname,
+					email: req.body.email,
+					password_hash: password_hash
+				});
+				return {
+					_code: 201,
+					lastname: user.lastname,
+					firstname: user.firstname,
+					email: user.email,
+					enabled: user.enabled
+				};
+			}
+			catch (err)
+			{
+				if (err.name && err.name === 'SequelizeUniqueConstraintError')
+					throw new APIError(409, 'Email in use');
+				throw err;
+			}
 		}
-	},
-	lastname: {
-		in: [ 'body' ],
-		isLength: {
-			errorMessage: 'Invalid lastname',
-			options: { min: 3, max: 30 }
-		}
-	},
-	firstname: {
-		in: [ 'body' ],
-		isLength: {
-			errorMessage: 'Invalid firstname',
-			options: { min: 3, max: 30 }
-		}
-	}
-}), (req, res) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty())
-		send(res, 400, { body: req.body, errors: errors.array() });
-	else
-		bcrypt.hash(req.body.password, config.bcrypt_rounds)
-			.then(password_hash => User.create({
-				lastname: req.body.lastname,
-				firstname: req.body.firstname,
-				email: req.body.email,
-				password_hash: password_hash
-			}))
-			.then(user => send(res, 201, {
-				lastname: user.lastname,
-				firstname: user.firstname,
-				email: user.email,
-				enabled: user.enabled
-			}))
-			.catch(err => (err.name && err.name === 'SequelizeUniqueConstraintError' ? send(res, 409, { message: 'Email already in use' }) : send(res, 500, err)));
-});
+	})
+);
