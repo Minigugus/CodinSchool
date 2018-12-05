114268c53a87f225874bcf8b2790b177d43617bc
rigwild <contact@asauvage.fr>
2018-12-05 13:57:04 +0100

    correction apollo

--- src/views/ApolloExampleView.vue
+++ src/views/ApolloExampleView.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <ApolloExample />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import ApolloExample from '@/components/ApolloExample.vue'
 
 export default {
-  name: 'home',
+  name: 'apolloExample',
   components: {
-    HelloWorld,
     ApolloExample
   }
 }
