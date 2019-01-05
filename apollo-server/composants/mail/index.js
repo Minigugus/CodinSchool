dda10ad1824a618b557ea40b8f1c42d83117ae7f
rigwild <contact@asauvage.fr>
2019-01-05 22:12:07 +0100

    Module mail: Export config, spread objet, template camelCase, meilleure doc

--- apollo-server/composants/mail/index.js
+++ apollo-server/composants/mail/index.js
@@ -2,35 +2,28 @@
 // Gestion de l'envoi des mails du serveur //
 // //////////////////////////////////////////
 
+import { SMTP, expediteurDefaut } from '../config'
 import nodemailer from 'nodemailer'
-
-const configNodemailer = {
-  host: process.env.mailSmtpHost,
-  port: 587,
-  secure: false,
-  auth: { user: process.env.mailSmtpLogin, pass: process.env.mailSmtpPass }
-}
-
-// Expéditeur par défaut
-const expediteurDefaut = { nom: 'CodinSchool', mail: 'no-reply@codinschool.fr' }
-
 
 // Liste des modèles de mail disponibles
 const listeModeles = {
   resetMdp: () => import('./modeles/resetMdp')
 }
-
 // Récupérer le contenu d'un modèle
 const getModele = fn => fn().then(x => x.default)
 
-// Remplacer les tags %donnée% d'un modèle par leurs vraies données
+/**
+ * Remplacer les tags %donnée% d'un modèle par leurs vraies données
+ *
+ * @param {string} modele Modèle de contenu avec valeurs à remplacer entre %
+ * @param {Object.<string, string>} remplacement Objet des données à remplacer
+ * @return {string} Le modèle avec ses données remplacées
+ */
 const remplacerDonnees = (modele, remplacement) => {
-  remplacement.forEach(x => {
-    modele = modele.replace(new RegExp(`%${x.nom}%`, 'g'), x.donnee)
-  })
+  // eslint-disable-next-line
+  for (const x in remplacement) modele = modele.replace(new RegExp(`%${x}%`, 'g'), remplacement[x])
   return modele
 }
-
 
 /**
  * Envoyer un mail à l'aide de nodemailer
@@ -42,7 +35,7 @@ const remplacerDonnees = (modele, remplacement) => {
  * @return {Promise<object>} Objet précisant le résultat de l'envoi du mail (nodemailer)
  */
 const envoyerMail = async ({ expediteur, destinataire, objet, contenu }) => {
-  const mailer = nodemailer.createTransport(configNodemailer)
+  const mailer = nodemailer.createTransport(SMTP)
   return mailer.sendMail({
     from: `"${expediteur.nom}" <${expediteur.mail}>`,
     to: `"${destinataire.nom}" <${destinataire.mail}>`,
@@ -52,15 +45,17 @@ const envoyerMail = async ({ expediteur, destinataire, objet, contenu }) => {
 }
 
 /**
- * Envoyer un mail en utilisant le modèle
+ * Envoyer un mail en utilisant le modèle `resetMdp.js`
  *
  * @param {{ nom: string, mail: string }} destinataire Données du destinataire
- * @param {Array<{ nom: string, donnee: string }>} remplacement Tableau des données à remplacer
+ * @param {Object} r Objet des données à remplacer
+ * @param {string} r.nomUtilisateur Nom d'utilisateur du destinataire
+ * @param {string} r.lienReset Lien de réinitialisation de mot de passe
  * @return {Promise<object>} Objet précisant le résultat de l'envoi du mail (nodemailer)
  */
-export const nouveauMdp = async (destinataire, remplacement) => {
+export const nouveauMdp = async (destinataire, r) => {
   const modele = await getModele(listeModeles.resetMdp)
-  const contenu = remplacerDonnees(modele, remplacement)
+  const contenu = remplacerDonnees(modele, r)
   return envoyerMail({
     expediteur: expediteurDefaut,
     destinataire,
