86b14b8b8609e6e2f60dcc51c375d84a3b847253
rigwild <contact@asauvage.fr>
2019-01-06 20:43:17 +0100

    petits fixs

--- apollo-server/composants/mail/mail.js
+++ apollo-server/composants/mail/mail.js
@@ -19,12 +19,13 @@ const SMTP = {
   auth: SMTP_UTILISATEUR && { user: SMTP_UTILISATEUR, pass: SMTP_MDP }
 }
 
-export const formaterMail = mailOuObjet => (
-  typeof mailOuObjet !== 'string' ? ('nom' in mailOuObjet)
-    ? `"${mailOuObjet.nom}" <${mailOuObjet.mail}>`
-    : `${mailOuObjet.mail}`
-    : `${mailOuObjet}`
-)
+/**
+ *  Formater un mail pour l'envoi
+ * @param {string|{ nom, mail }} mailOuObjet données à traiter
+ * @return {string} String formatée pour l'envoi
+ */
+export const formaterMail = mailOuObjet =>
+  typeof mailOuObjet === 'string' ? mailOuObjet : `"${mailOuObjet.nom}" <${mailOuObjet.mail}>`
 
 /**
  * Envoyer un mail à l'aide de nodemailer
