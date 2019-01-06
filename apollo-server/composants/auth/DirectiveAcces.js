0e699cc0ebb6013248223f19195252c5506669f1
Florentin Magniez <43109623+Minigugus@users.noreply.github.com>
2019-01-06 14:16:11 +0100

    Base de donnees (#7)
    Intégration de la base de données.
    * Refactoring partie Serveur + Début Intégration Sequelize
    
    * Intégration PostgreSQL + Refactoring terminés
    
    * Intégration PostgreSQL + Refactoring terminés
    
    * Linted

--- apollo-server/composants/auth/DirectiveAcces.js
+++ apollo-server/composants/auth/DirectiveAcces.js
@@ -2,36 +2,23 @@
 // Gestion des restrictions d'accès //
 // ////////////////////////////////////
 
-import { promisify } from 'util'
-import { sign, verify } from 'jsonwebtoken'
 import { defaultFieldResolver } from 'graphql'
 import { SchemaDirectiveVisitor } from 'graphql-tools'
 
-import bdd from '../bdd'
-
-import { SECRET_JWT, DUREE_VALIDITE_JETON } from '../composants/config'
-
-const jwt = {
-  sign: promisify(sign),
-  verify: promisify(verify)
-}
+import { AuthentificationRequiseError, AccesInterditError } from './erreurs'
 
 const TYPE_REQUIS = Symbol()
 const DEJA_PROTEGE = Symbol()
 
 export default class DirectiveAcces extends SchemaDirectiveVisitor {
-  static creerJeton(utilisateur) {
-    return jwt.sign(utilisateur, SECRET_JWT, { expiresIn: DUREE_VALIDITE_JETON })
-  }
-
   visitObject(type) {
     this.assurerChampsProteges(type)
-    type[TYPE_REQUIS] = this.args.requis
+    type[TYPE_REQUIS] = this.args.interdit
   }
 
   visitFieldDefinition(champ, details) {
     this.assurerChampsProteges(details.objectType)
-    champ[TYPE_REQUIS] = this.args.requis
+    champ[TYPE_REQUIS] = this.args.interdit
   }
 
   assurerChampsProteges(type) {
@@ -41,29 +28,15 @@ export default class DirectiveAcces extends SchemaDirectiveVisitor {
       Object.keys(champs).forEach(nomChamp => {
         const champ = champs[nomChamp]
         const resoudre = champ.resolve || defaultFieldResolver
+        const rolesInterdits = (champ[TYPE_REQUIS] || []).concat(type[TYPE_REQUIS] || [])
         champ.resolve = async function (...args) {
-          if (!champ[TYPE_REQUIS] && !type[TYPE_REQUIS]) return resoudre.apply(this, args)
-
-          let utilisateur = args[2].utilisateur
-          const permissionsRequises = (champ[TYPE_REQUIS] || []).concat(type[TYPE_REQUIS] || [])
-          if (!utilisateur) {
-            const authentization = args[2].headers.authorization
-            if (authentization && /^Bearer /.test(authentization)) {
-              try {
-                utilisateur = await jwt.verify(authentization.slice(7), SECRET_JWT)
-                utilisateur = bdd.recupererUtilisateur(utilisateur.id)
-                utilisateur.permissions = bdd.recupererPermissions(utilisateur)
-              }
-              catch (err) {
-                throw new Error('Jeton de connexion invalide.')
-              }
-            }
-            else throw new Error('Cette requête requière une authentification.')
-            args[2].utilisateur = utilisateur
+          if (champ[TYPE_REQUIS] || type[TYPE_REQUIS]) {
+            const utilisateur = args[2].utilisateur
+            if (!utilisateur) throw new AuthentificationRequiseError()
+            else if (rolesInterdits.includes(utilisateur.role))
+              throw new AccesInterditError(utilisateur.role, rolesInterdits)
           }
-          if (permissionsRequises.every(x => utilisateur.permissions.has(x)))
-            return resoudre.apply(this, args)
-          throw new Error('Droits d\'accès insuffisants.')
+          return resoudre.apply(this, args)
         }
       })
     }
