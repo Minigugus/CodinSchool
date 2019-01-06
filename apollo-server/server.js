116e9629d4a08e6db8b38fbc7a4ca720f644f822
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2019-01-06 17:24:38 +0100

    Ajout prise en charge `vue-cli-plugin-apollo` (permet `npm run apollo:run`)

--- apollo-server/server.js
+++ apollo-server/server.js
@@ -1,9 +1,11 @@
 import path from 'path'
 import express from 'express'
 
+import bdd from './composants/bdd'
 import { SERVIR_FICHIERS_STATIQUES } from './composants/config'
 
 export default app => {
   // app.use('/files', express.static(path.resolve(__dirname, '../live/uploads')))
   if (SERVIR_FICHIERS_STATIQUES) app.use('/', express.static(path.resolve(__dirname, '../dist')))
+  return bdd.sync()
 }
