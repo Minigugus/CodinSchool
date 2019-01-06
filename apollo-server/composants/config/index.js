cd4d2be1c06f4506de337a6dafb66140431d749f
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2019-01-06 17:24:06 +0100

    Désactivation des Mocks par défaut

--- apollo-server/composants/config/index.js
+++ apollo-server/composants/config/index.js
@@ -1,6 +1,6 @@
-// //////////////////////////////////////////
+// ////////////////////////////////////// //
 // Gestion de la configuration du serveur //
-// //////////////////////////////////////////
+// ////////////////////////////////////// //
 
 export const MODE_DEVELOPPEMENT = process.env.NODE_ENV !== 'production'
 
@@ -9,19 +9,24 @@ if (!MODE_DEVELOPPEMENT && !process.env.SECRET_JWT)
     'En mode production, vous devez spécifier un secret via la variable d\'environnement SECRET_JWT.'
   )
 
-// Réseau
+// DÉVELOPPEMENT //
+// NOTE : Peut être source de bugs quand activé.
+export const ACTIVER_MOCKS = (process.env.ACTIVER_MOCKS === 'oui')
+
+// RÉSEAU //
 export const PORT = process.env.PORT || (MODE_DEVELOPPEMENT ? 4000 : 80)
 
-// Jeton de session JWT
+// JETON DE SESSION JWT //
+// TODO : Utiliser un clé privée et une clé publique plutôt qu'on secret.
 export const SECRET_JWT = process.env.SECRET_JWT || 'modeDeveloppement'
 export const DUREE_VALIDITE_JETON = '1d'
 
-// Base de Données
+// BASE DE DONNÉES //
 export const BDD_URI = process.env.BDD_URI || 'postgres://codinschool:codinschool@bdd/codinschool'
 
-// BCrypt (mots de passe)
+// BCRYPT (MOTS DE PASSE) //
 export const BCRYPT_ROUND = process.env.BCRYPT_ROUND || 10
 
-// Fichiers statiques
+// FICHIERS STATIQUES //
 export const SERVIR_FICHIERS_STATIQUES =
-  process.env.SERVIR_FICHIERS_STATIQUES || !MODE_DEVELOPPEMENT
+  !!process.env.SERVIR_FICHIERS_STATIQUES || !MODE_DEVELOPPEMENT
