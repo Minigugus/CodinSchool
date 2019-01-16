bfb74351dc8a7a46fe296f79eaa2a93229db9709
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2019-01-16 15:54:51 +0100

    Fix #11

--- apollo-server/composants/utilisateur/index.js
+++ apollo-server/composants/utilisateur/index.js
@@ -4,6 +4,6 @@ export const Profile = _Profile
 
 export const recupererParID = id => Profile.findByPk(id)
 export const recupererParEmail = email =>
-  Profile.findOne({ where: { emailPrimaire: { in: [email] } } })
+  Profile.findOne({ where: { emailPrimaire: { in: [email.toLowerCase()] } } })
 export const recupererParValidation = validation =>
   Profile.findOne({ where: { validationInscription: { in: [validation] } } })
