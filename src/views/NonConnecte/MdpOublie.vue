c1bb029d9af4546801ec6a8b089d7b5ec89a2bc3
TheArKXIV <arkangefr@outlook.com>
2019-01-17 15:26:59 +0100

    Completion de la page Mot de passe Oublié

--- src/views/NonConnecte/MdpOublie.vue
+++ src/views/NonConnecte/MdpOublie.vue
@@ -2,9 +2,22 @@
   <div class="ui container segment stripe">
     <h2 class="ui center aligned header">
       <div class="content">
-        Mot de passe oublié
+        Mot de passe oublié?
       </div>
     </h2>
+    <div class="contenu">
+      <div class="formulaire">
+        <form class="ui form">
+          <p>Entrez votre adresse Mail pour recevoir un lien vous permettant de reinitialiser votre mot de passe.</p>
+          <div class="field required">
+            <label>Adresse Mail</label>
+            <input type="text" name="email" placeholder="Adresse Mail" required oninvalid="this.setCustomValidity('Veuillez indiquer une adresse Email')">
+          </div>
+          <button class="ui button" type="submit">Envoyer</button>
+        </form>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -20,5 +33,18 @@ export default {
 </script>
 
 <style scoped>
-/* Style de la page */
+   .contenu{
+        padding: 20px;
+
+    }
+    .formulaire{
+        padding: 20px;
+        top: 20px;
+        width:40%;
+        margin: auto;
+        border-radius: 15px;
+    }
+    .formulaire h1, .formulaire p{
+        text-align: center;
+    }
 </style>
