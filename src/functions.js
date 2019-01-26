6de12348425b99065494a5ee3cee30b9ccdc3d94
rigwild <contact@asauvage.fr>
2019-01-26 15:49:01 +0100

    Ajout id exercices + edition de niveau

--- src/functions.js
+++ src/functions.js
@@ -3,24 +3,28 @@
 // Fausse liste de niveaux en attendant que le schema GraphQL du serveur soit réalisé
 const fakeListeNiveau = [
   {
-    id: 1,
-    nom: 'Niveau 1',
-    description: 'Description niveau 1',
+    id: 'niveau-un',
+    nom: 'Premier niveau',
+    description: 'Les bases de l\'algorithmie.',
     exercice: [
       {
         id: 1,
         nom: 'Exercice 1.1',
-        description: 'Description exercice 1.1'
+        description: 'Description exercice 1.1',
+        correction: 'Correction exercice 1.1'
       },
       {
-        id: 2,
-        nom: 'Exercice 1.2',
-        description: 'Description exercice 1.2'
+        id: 'mon-super-exercice',
+        nom: 'Les boucles',
+        description: 'Sortir tous les nombres de 1 à 10 séparés par un retour à la ligne.',
+        correction:
+          '#include <stdio.h>\nint main(void) {\n  int i;\n  for (i = 0 ; i < 10 ; ++i)\n    printf("%d\\n", i);\n  return 0;\n}'
       },
       {
         id: 5,
         nom: 'Exercice 1.3',
-        description: 'Description exercice 1.3'
+        description: 'Description exercice 1.3',
+        correction: 'Correction exercice 1.3'
       }
     ]
   },
@@ -32,12 +36,14 @@ const fakeListeNiveau = [
       {
         id: 3,
         nom: 'Exercice 2.1',
-        description: 'Description exercice 2.1'
+        description: 'Description exercice 2.1',
+        correction: 'Correction exercice 2.1'
       },
       {
         id: 6,
         nom: 'Exercice 2.2',
-        description: 'Description exercice 2.2'
+        description: 'Description exercice 2.2',
+        correction: 'Correction exercice 2.2'
       }
     ]
   },
@@ -49,12 +55,14 @@ const fakeListeNiveau = [
       {
         id: 9,
         nom: 'Exercice 3.1',
-        description: 'Description exercice 3.1'
+        description: 'Description exercice 3.1',
+        correction: 'Correction exercice 3.1'
       },
       {
         id: 4,
         nom: 'Exercice 3.2',
-        description: 'Description exercice 3.2'
+        description: 'Description exercice 3.2',
+        correction: 'Correction exercice 3.2'
       }
     ]
   }
