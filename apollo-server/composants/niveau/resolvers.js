6ff84eaf73fd4d2fc70f7393d62f2e77a0c2ee92
MAGNIEZ Florentin <florentin.magniez@etu.univ-littoral.fr>
2019-01-31 20:51:38 +0100

    Ajout Gestion Erreurs Niveau + Refactoring

--- apollo-server/composants/niveau/resolvers.js
+++ apollo-server/composants/niveau/resolvers.js
@@ -1,61 +1,33 @@
-import Sequelize from 'sequelize'
-import Niveau from './Niveau'
-import bdd from '../bdd'
+import { recupererTous, recupererParID, creerNiveau, editerNiveau, reorganiserNiveaux, supprimerNiveau, recupererExercicesNiveau } from '.'
 
 export default {
   Query: {
     niveaux() {
-      return Niveau.findAll({ order: [['position', 'ASC']] })
+      return recupererTous()
     },
-    async niveau(_, { id }) {
-      const niveau = Niveau.findByPk(id)
-      if (!niveau)
-        throw new Error(`Le Niveau « ${id} » n'existe pas.`)
-      return niveau
+    niveau(_, { id }) {
+      return recupererParID(id)
     }
   },
   Mutation: {
     creerNiveau(_, { niveau }) {
-      return bdd.transaction(async transaction => {
-        const dernierNiveau = await Niveau.max('position', { transaction })
-        niveau.position = dernierNiveau + 1 || 0
-        return Niveau.create(niveau, { transaction })
-      })
+      return creerNiveau(niveau)
     },
-    async editerNiveau(_, { id, niveau }) {
-      const affecte = await Niveau.update(niveau, { where: { id } })
-      if (!affecte) throw new Error(`Le Niveau « ${id} » n'existe pas.`)
-      return await Niveau.findByPk(id)
+    editerNiveau(_, { id, niveau }) {
+      return editerNiveau(id, niveau)
     },
-    async reorganiserNiveaux(_, { niveaux }) {
-      await bdd.transaction(async transaction => {
-        const nombreNiveaux = await Niveau.count({ transaction })
-        if (niveaux.length < nombreNiveaux)
-          throw new Error('Vous devez réordoner tous les niveaux.')
-        else if (niveaux.length > nombreNiveaux) throw new Error('Au moins un Niveau n\'existe pas.')
-        return Promise.all(
-          niveaux.map((id, position) => Niveau.update({ position }, { where: { id }, transaction }))
-        )
-      })
-      // await Niveau.update(niveaux.map((id, position) => ({ id, position })), { where: { } });
-      return await Niveau.findAll({
-        where: {
-          id: { [Sequelize.Op.in]: niveaux }
-        },
-        order: [['position', 'ASC']]
-      })
+    reorganiserNiveaux(_, { niveaux }) {
+      return reorganiserNiveaux(niveaux).then(() => recupererTous())
     },
-    async supprimerNiveau(_, { id }) {
-      const affecte = await Niveau.destroy({ where: { id } })
-      if (!affecte) throw new Error(`Le Niveau « ${id} » n'existe pas.`)
-      return id
+    supprimerNiveau(_, { id }) {
+      return supprimerNiveau(id)
     }
   },
 
   Niveau: {
     exercices(niveau) {
       // TODO : Cacher les exercices aux étudiants qui n'ont pas débloqué le niveau précédent.
-      return niveau.getExercices({ order: [['position', 'ASC']] })
+      return recupererExercicesNiveau(niveau)
     }
   }
 }
