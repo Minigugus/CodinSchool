e9aef9f26603c75a126baa527d73e88435fa8c07
MAGNIEZ Florentin <Florentin.Magniez@etu.univ-littoral.fr>
2019-01-31 09:32:19 +0100

    Début intégration Niveaux et Exercices

--- apollo-server/resolvers.js
+++ apollo-server/resolvers.js
@@ -1,5 +1,13 @@
 import deepmerge from 'deepmerge'
 
+import Niveau from './composants/niveau/resolvers.js'
+import Exercice from './composants/exercice/resolvers.js'
 import Authentification from './composants/auth/resolvers.js'
 
-export default deepmerge.all([{ Query: {}, Mutation: {} }, Authentification])
+export default deepmerge.all([
+  { Query: {}, Mutation: {} },
+
+  Niveau,
+  Exercice,
+  Authentification
+])
