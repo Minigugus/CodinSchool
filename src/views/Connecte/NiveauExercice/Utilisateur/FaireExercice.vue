8b3e6c9521916c3cbea6877923108eee303fe67d
rigwild <contact@asauvage.fr>
2019-02-20 19:08:52 +0100

    exemple code avec sélection de coloration syntaxque Reorganisation de router compression données codemirorr

---
+++ src/views/Connecte/NiveauExercice/Utilisateur/FaireExercice.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="ui container segment">
+    <div class="ui container mb-2">
+      <sui-dropdown
+        v-model="langageProgrammation"
+        fluid
+        :options="options"
+        placeholder="Liste des langages"
+        search
+        selection
+      />
+    </div>
+
+    <form-champs
+      v-model="champs.code.v"
+      tag="codeeditor"
+      nom="Editeur de code"
+      id="editeurCode"
+      :type="langageProgrammation"
+      :err="champs.code.err"
+    />
+  </div>
+</template>
+
+<script>
+import FormChamps from '@/components/FormChamps.vue'
+import { codeMirrorLanguages } from '@/codeMirrorLanguagesLoader'
+
+const options = Object.keys(codeMirrorLanguages).map(key => {
+  return {
+    text: key,
+    value: key
+  }
+})
+
+export default {
+  name: 'FaireExercice',
+  components: {
+    FormChamps
+  },
+
+  data() {
+    return {
+      langageProgrammation: 'JavaScript',
+      options,
+
+      champs: {
+        code: { v: '', err: [] }
+      }
+    }
+  }
+}
+</script>
