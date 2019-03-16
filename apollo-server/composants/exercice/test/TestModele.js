9ec1adb4322fa874ca05447bf11ef9d02c1af941
Minigugus <Florentin.Magniez@etu.univ-littoral.fr>
2019-03-16 15:46:02 +0100

    Début ajout Tests & Soumission

---
+++ apollo-server/composants/exercice/test/TestModele.js
@@ -0,0 +1,34 @@
+import Sequelize from 'sequelize'
+import bdd from '../../bdd'
+
+const Test = bdd.define(
+  'test',
+  {
+    id: {
+      allowNull: false,
+      type: Sequelize.STRING(31),
+      primaryKey: true
+    },
+
+    nom: {
+      allowNull: false,
+      type: Sequelize.STRING(256)
+    },
+
+    entree: {
+      allowNull: false,
+      type: Sequelize.STRING(20000)
+    },
+
+    sortie: {
+      allowNull: false,
+      defaultValue: '',
+      type: Sequelize.STRING(20000)
+    }
+  },
+  {
+    timestamps: false
+  }
+)
+
+export default Test
